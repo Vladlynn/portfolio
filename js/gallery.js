066cc355bfdac1c89ddd1f0e02503ddebf757b45
Vladlynn <palashevskiy.vv@gmail.com>
2025-03-16 20:16:29 +0100

    changes

--- js/gallery.js
+++ js/gallery.js
@@ -1,1 +1,57 @@
-let closeBtn=document.querySelector(".close-button"),glassBackground=document.querySelector(".glass-background"),graphicLink=document.querySelector(".graphic-design-link"),galleryGraphic=document.querySelector(".gallery-graphic-flex"),webLink=document.querySelector(".web-design-link"),galleryWeb=document.querySelector(".gallery-web-flex");function openGraphicGallery(e){e.preventDefault(),glassBackground.style.zIndex="1",glassBackground.style.opacity="1",galleryGraphic.style.left="calc((100vw - 70vw) / 2)"}function openWebGallery(e){e.preventDefault(),glassBackground.style.zIndex="1",glassBackground.style.opacity="1",galleryWeb.style.left="calc((100vw - 70vw) / 2)"}graphicLink&&graphicLink.addEventListener("click",openGraphicGallery),closeBtn&&closeBtn.addEventListener("click",()=>{glassBackground.style.zIndex="-1",glassBackground.style.opacity="0",galleryGraphic.style.left="-170vw"}),webLink&&webLink.addEventListener("click",openWebGallery),closeBtn&&closeBtn.addEventListener("click",()=>{glassBackground.style.zIndex="-1",glassBackground.style.opacity="0",galleryWeb.style.left="-170vw"});
+let closeBtn = document.querySelector('.close-button');
+let glassBackground = document.querySelector('.glass-background');
+
+
+let graphicLink = document.querySelector('.graphic-design-link');
+let galleryGraphic = document.querySelector('.gallery-graphic-flex');
+
+let webLink = document.querySelector('.web-design-link');
+let galleryWeb = document.querySelector('.gallery-web-flex');
+
+//========================================================================
+function openGraphicGallery(event) {
+    event.preventDefault(); // Предотвращаем переход по ссылке
+    glassBackground.style.zIndex = '1';
+    glassBackground.style.opacity = '1';
+    galleryGraphic.style.left = 'calc((100vw - 70vw) / 2)';
+}
+
+// Добавляем обработчики кликов на ссылки
+if (graphicLink) {
+    graphicLink.addEventListener('click', openGraphicGallery);
+}
+
+
+// Закрытие галереи
+if (closeBtn) {
+    closeBtn.addEventListener('click', () => {
+        glassBackground.style.zIndex = '-1';
+        glassBackground.style.opacity = '0';
+        galleryGraphic.style.left = '-170vw'; 
+    });
+}
+
+//========================================================================
+
+function openWebGallery(event) {
+    event.preventDefault(); // Предотвращаем переход по ссылке
+    glassBackground.style.zIndex = '1';
+    glassBackground.style.opacity = '1';
+    galleryWeb.style.left = 'calc((100vw - 70vw) / 2)';
+}
+
+// Добавляем обработчики кликов на ссылки
+
+
+if (webLink) {
+    webLink.addEventListener('click', openWebGallery);
+}
+
+// Закрытие галереи
+if (closeBtn) {
+    closeBtn.addEventListener('click', () => {
+        glassBackground.style.zIndex = '-1';
+        glassBackground.style.opacity = '0';
+        galleryWeb.style.left = '-170vw'; 
+    });
+}
